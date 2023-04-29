7934d580f33f01abb0404bb98ac4291c627c52e3
spallya <spallya@outlook.com>
2023-04-30 04:56:45 +0530

    Login Page - StatefulWidget, Animation, Delay

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool buttonChanged = false;
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +26,9 @@ class LoginPage extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            const Text(
-              "Welcome",
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            Text(
+              "Welcome $name",
+              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
             ),
             const SizedBox(
               height: 20,
@@ -33,6 +41,10 @@ class LoginPage extends StatelessWidget {
                   TextFormField(
                     decoration: const InputDecoration(
                         hintText: "Enter username", labelText: "Username"),
+                    onChanged: (userNmae) {
+                      name = userNmae;
+                      setState(() {});
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -42,14 +54,40 @@ class LoginPage extends StatelessWidget {
                   const SizedBox(
                     height: 30,
                   ),
-                  ElevatedButton(
-                      onPressed: () => {
-                            Navigator.pushNamed(
-                                context, CatalogAppRoutes.homeRoute)
-                          },
-                      style: TextButton.styleFrom(
-                          minimumSize: const Size(150, 40)),
-                      child: const Text("Login"))
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        buttonChanged = true;
+                      });
+                      await Future.delayed(const Duration(milliseconds: 350));
+                      Navigator.pushNamed(context, CatalogAppRoutes.homeRoute);
+                    },
+                    child: AnimatedContainer(
+                      duration: const Duration(milliseconds: 250),
+                      width: buttonChanged ? 45 : 150,
+                      height: 45,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: buttonChanged ? Colors.green : Colors.indigo,
+                          borderRadius:
+                              BorderRadius.circular(buttonChanged ? 45 : 8)),
+                      child: buttonChanged
+                          ? const Icon(Icons.done, color: Colors.white)
+                          : const Text("Login",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18)),
+                    ),
+                  )
+                  // ElevatedButton(
+                  //     onPressed: () => {
+                  //           Navigator.pushNamed(
+                  //               context, CatalogAppRoutes.homeRoute)
+                  //         },
+                  //     style: TextButton.styleFrom(
+                  //         minimumSize: const Size(150, 40)),
+                  //     child: const Text("Login"))
                 ],
               ),
             )
